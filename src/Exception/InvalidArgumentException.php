c076013898236c4dbecf58e89e7ec70b830835cd
George Steel <george@net-glue.co.uk>
2018-08-20 16:57:12 +0100

    Delete unused exception classes

--- src/Exception/InvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace NetglueRealIP\Exception;
-
-class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
-{
-
-}
--- src/Exception/RuntimeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace NetglueRealIP\Exception;
-
-class RuntimeException extends \RuntimeException implements ExceptionInterface
-{
-
-}
